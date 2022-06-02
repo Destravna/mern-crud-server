cd4a7a36321291a8c209c879a7d95cae91c084f3
Dhruv <kingrajput450@gmail.com>
2022-06-02 16:29:49 +0530

    second commit

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 require("./db/conn.js");
+require("dotenv").config();
+
 app.use(bodyParser.urlencoded({extended: true}));
-const port = 8003;
+const port = 8003 || process.env.PORT;
 const users = require("./model/userSchema");
 const router = require("./routes/router");
 
